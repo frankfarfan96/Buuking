d2074616bf987fb8489654429a0647ece7852340
Frank Farfan <frankfarfan96@gmail.com>
2022-09-06 21:53:58 +0200

    Added folder components with some components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 };
 
 export default Home;
