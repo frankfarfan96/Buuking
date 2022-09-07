6e7784d7b46abd9015b4f042e0019c87b984f580
Frank Farfan <frankfarfan96@gmail.com>
2022-09-07 10:23:37 +0200

    Implemented the Calendar in Header file and installed another neccesary library date-fns

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,8 +8,18 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
 
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+
   return (
     <div className="header">
       <div className="headerContainer">
@@ -53,6 +63,10 @@ const Header = () => {
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
             <span className="headerSearchText">date to date</span>
+            edotableDateInputs={true}
+            onChange={(item) => setDate([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={date}
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className="headerIcon" />
