cbf249d9ebdc081a2d46a2ecde08889172bbac65
Frank Farfan <frankfarfan96@gmail.com>
2022-09-07 09:59:54 +0200

    Added Search design in the Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,6 @@
 import {
+  faPerson,
+  faCalendarDays,
   faBed,
   faPlane,
   faCar,
@@ -39,6 +41,27 @@ const Header = () => {
           with a free FFbuuking account
         </p>
         <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
