ca433b011ba159cc1d2fbfcfa828b8df6b548351
Frank Farfan <frankfarfan96@gmail.com>
2022-09-07 19:07:13 +0200

    Finished Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,53 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Distric</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Houses</li>
+          <li className="fListItem">Apartments</li>
+          <li className="fListItem">Resorts</li>
+          <li className="fListItem">Villas</li>
+          <li className="fListItem">Hostels</li>
+          <li className="fListItem">Bed and Breakfasts</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Hostels and pensions</li>
+          <li className="fListItem">Unique accommodations</li>
+          <li className="fListItem">All destinations</li>
+          <li className="fListItem">Discover</li>
+          <li className="fListItem">Opinions</li>
+          <li className="fListItem">Articles</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Traveling Communities</li>
+          <li className="fListItem">Seasonal and holiday offers</li>
+          <li className="fListItem">Car rental</li>
+          <li className="fListItem">Flight search engine</li>
+          <li className="fListItem">Restaurant reservations</li>
+          <li className="fListItem">Customer Support</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">About coronavirus (COVID-19)</li>
+          <li className="fListItem">About FFBuuking</li>
+          <li className="fListItem">Careers</li>
+          <li className="fListItem">Help for collaborators</li>
+          <li className="fListItem">Corporate Contact</li>
+          <li className="fListItem">Terms and Conditions</li>
+        </ul>
+      </div>
+      <div className="fText">Copyright © 2022 - FFBuuking</div>
+    </div>
+  );
+};
+
+export default Footer;
