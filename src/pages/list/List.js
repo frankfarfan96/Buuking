9a71dac8ea5da33b9f8fedd00387ec1acd580617
Frank Farfan <frankfarfan96@gmail.com>
2022-09-07 12:08:11 +0200

    Added List page layout

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <>
+      <Navbar />
+      <Header type="list" />
+    </>
+  );
 };
 
 export default List;
