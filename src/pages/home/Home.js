35a459cba3812ed4dc1015c99f1ce3c8c82cc178
Frank Farfan <frankfarfan96@gmail.com>
2022-09-07 18:35:24 +0200

    Added new component FeaturedProperties in the Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,9 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return (
@@ -11,6 +13,9 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </>
   );
