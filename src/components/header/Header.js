085b762e746a9423a23b616a988829e9b9466aa1
Frank Farfan <frankfarfan96@gmail.com>
2022-09-08 11:07:05 +0200

    Added Calendar in List Page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -112,6 +112,7 @@ const Header = ({ type }) => {
                     moveRangeOnFirstSelection={false}
                     ranges={date}
                     className="date"
+                    minDate={new Date()}
                   />
                 )}
               </div>
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -3,9 +3,16 @@ import "./list.css";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
   const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
 
   console.log(location);
   return (
@@ -18,11 +25,21 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Destination</label>
-              <input type="text" />
+              <input placeholder={`${destination}`} type="text" />
             </div>
             <div className="lsItem">
               <label>Check-in Date </label>
-              <input type="text" />
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yyyy"
+              )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
             </div>
           </div>
           <div className="listResult"></div>
