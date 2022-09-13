76738413d9d65e298b83b1082de6c55e0373b7f2
Frank Farfan <frankfarfan96@gmail.com>
2022-09-13 10:11:48 +0200

    Added Linked for Logo in Navbar to Homepage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,13 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">FF Buuking</span>
+        <Link to="/" className="logo">
+          <span>FF Buuking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
